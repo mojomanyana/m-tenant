fbca8314b2dbed250430631925474e4be7a12838
mojomanyana <nemanja@fabrika.nyc>
2018-04-26 14:19:59 +0200

    Again structural formatting 16

--- servereless-tenant-api/src/index.js
+++ servereless-tenant-api/src/index.js
@@ -1,5 +1,5 @@
 import { success } from '../../_shared/labda/responses';
 
 exports.get = (event, context, callback) => {
-  callback(null, success({ data: 'Success2' }));
+  callback(null, success({ data: 'Success' }));
 };
