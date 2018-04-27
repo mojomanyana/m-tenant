436e956d22588600bf02daa09cc61095f2e4d649
mojomanyana <nemanja@fabrika.nyc>
2018-04-27 13:05:13 +0200

    coverage added

---
+++ _bin/make_coverage.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+function coverage {
+  (
+    cd $1;\
+    npm run coverage;\
+    cd ..;\
+  )
+}
+
+for d in */ ; do
+    if [ $d != "_bin/" ] && [ $d != "node_modules/" ] && [ $d != "_shared/" ]; then
+        echo -e "\x1b[32m*** $d lcov coverage report ***\x1b[39m";
+        coverage $d;
+    fi
+done
