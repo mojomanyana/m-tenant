b5663031547bcd3dfa665f968c15f4773559c7ef
mojomanyana <nemanja@fabrika.nyc>
2018-04-28 00:54:37 +0200

    changes

--- _bin/make_coverage.sh
+++ _bin/make_coverage.sh
@@ -3,10 +3,15 @@
 function coverage {
   (
     cd $1;\
-    npm run test-to-report;\
+    npm run report;\
     cd ..;\
+    mkdir -p tmp/;\
+    ./cc-test-reporter format-coverage -t lcov -o tmp/$1api.codeclimate.json ./$1coverage/lcov.info;\
   )
 }
+#curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-darwin-amd64 > ./cc-test-reporter;\
+chmod +x ./cc-test-reporter;\
+#./cc-test-reporter before-build;\
 
 for d in */ ; do
     if [ $d != "_bin/" ] && [ $d != "node_modules/" ] && [ $d != "_shared/" ]; then
@@ -14,3 +19,6 @@ for d in */ ; do
         coverage $d;
     fi
 done
+./cc-test-reporter sum-coverage tmp/*/api.codeclimate.json -o tmp/codeclimate.total.json
+./cc-test-reporter upload-coverage -i tmp/codeclimate.total.json -r fad47c53ea9b61aa77a5347330971e561ba5bfcf38cb075b3e08ade43f22ddf8
+#./cc-test-reporter after-build
