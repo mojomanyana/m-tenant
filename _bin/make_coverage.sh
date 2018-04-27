8bf341181df2f043c52460b42456c128e6ba07b2
mojomanyana <nemanja@fabrika.nyc>
2018-04-28 01:07:33 +0200

    tmp removed

--- _bin/make_coverage.sh
+++ _bin/make_coverage.sh
@@ -11,7 +11,6 @@ function coverage {
 }
 curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-darwin-amd64 > ./cc-test-reporter;\
 chmod +x ./cc-test-reporter;\
-#./cc-test-reporter before-build;\
 
 for d in */ ; do
     if [ $d != "_bin/" ] && [ $d != "node_modules/" ] && [ $d != "_shared/" && [ $d != "tmp/" ]]; then
@@ -21,4 +20,5 @@ for d in */ ; do
 done
 ./cc-test-reporter sum-coverage tmp/*/api.codeclimate.json -o tmp/codeclimate.total.json
 ./cc-test-reporter upload-coverage -i tmp/codeclimate.total.json -r fad47c53ea9b61aa77a5347330971e561ba5bfcf38cb075b3e08ade43f22ddf8
-#./cc-test-reporter after-build
+
+rm -r tmp;\
