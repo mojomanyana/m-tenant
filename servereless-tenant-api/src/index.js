ffe5e4aeacc6091024e6556a995f7e88dc1b5869
mojomanyana <nemanja@fabrika.nyc>
2018-04-28 15:29:40 +0200

    Get tenants serverless API initial work done

--- servereless-tenant-api/src/index.js
+++ servereless-tenant-api/src/index.js
@@ -1,5 +1,36 @@
-import { success } from '../../_shared/labda/responses';
+import dotEnv from 'dotenv';
+import { success, failure } from '../../_shared/labda/responses';
+import { call } from '../../_shared/labda/dynamodb-helper';
+
+dotEnv.config();
 
 exports.get = (event, context, callback) => {
-  callback(null, success({ data: 'Success' }));
+  try {
+    const { size } = event.queryStringParameters || { size: 100 };
+    const paramsBody = JSON.parse(event.body);
+    const getTenantsListParams = {
+      TableName: process.env.DYNAMODB_TENANT_TABLE,
+      IndexName: 'createdAt-index',
+      KeyConditionExpression: 'createdAt > :crtAt',
+      ExpressionAttributeValues: {
+        ':crtAt': 0,
+      },
+      Limit: size,
+      ScanIndexForward: false,
+      ExclusiveStartKey: paramsBody.lastEvaluatedKey,
+    };
+    call(getTenantsListParams)
+      .then(data =>
+        callback(
+          null,
+          success({
+            status: true,
+            tenants: data.Items,
+            lastEvaluatedKey: data.LastEvaluatedKey,
+          }),
+        ))
+      .catch(err => callback(null, failure({ status: false, error: err })));
+  } catch (err) {
+    callback(null, failure({ status: false, error: err }));
+  }
 };
