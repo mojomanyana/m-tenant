2934b038bc86779848cde6ca099972b2ddcd958f
mojomanyana <nemanja@fabrika.nyc>
2018-05-03 14:06:13 +0200

    Some changes in file structure

--- _bin/make_test.sh
+++ _bin/make_test.sh
@@ -8,6 +8,10 @@ function testing {
   )
 }
 
+set -o allexport
+source ./.env
+set +o allexport
+
 for d in */ ; do
     if [ "$d" != "_bin/" ] && [ "$d" != "node_modules/" ] && [ "$d" != "_shared/" ] && [ "$d" != "tmp/" ]; then
         echo -e "\x1b[32m*** $d testing ***\x1b[39m";
