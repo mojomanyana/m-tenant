3264c4bf41d08a49cadf3e75b987cfff394f0c85
mojomanyana <nemanja@fabrika.nyc>
2018-05-04 11:48:03 +0200

    Added initial work for Save new tenant API support #4

--- _shared/labda/responses-test-helper.js
+++ _shared/labda/responses-test-helper.js
@@ -1,7 +1,7 @@
 const check = (response, expect, code) => {
   expect(response.statusCode).to.be.a('number');
   expect(response.statusCode).to.equal(code);
-}
+};
 
 module.exports = {
   errorResponseCheck: (response, expect) => check(response, expect, 500),
