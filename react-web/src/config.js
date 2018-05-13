8e96b15aba28d8be89917150b3a52db6d1d56a3b
mojomanyana <nemanja@fabrika.nyc>
2018-05-13 22:36:58 +0200

    Initial client react app added 2

--- react-web/src/config.js
+++ react-web/src/config.js
@@ -1,29 +1,29 @@
 const dev = {
-  STRIPE_KEY: "pk_test_v1amvR35uoCNduJfkqGB8RLD",
+  STRIPE_KEY: 'pk_test_v1amvR35uoCNduJfkqGB8RLD',
   apiGateway: {
-    REGION: "us-east-1",
-    URL: "https://di7nan8s01.execute-api.us-east-1.amazonaws.com/Prod"
+    REGION: 'us-east-1',
+    URL: 'https://di7nan8s01.execute-api.us-east-1.amazonaws.com/Prod',
   },
   cognito: {
-    REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_f7f94AC0F",
-    APP_CLIENT_ID: "6u3ukkptov5uksoklarhcmsul8",
-    IDENTITY_POOL_ID: "us-east-1:8a592fb0-5fd7-400f-9ccc-ab5be2638256"
-  }
+    REGION: 'us-east-1',
+    USER_POOL_ID: 'us-east-1_f7f94AC0F',
+    APP_CLIENT_ID: '6u3ukkptov5uksoklarhcmsul8',
+    IDENTITY_POOL_ID: 'us-east-1:8a592fb0-5fd7-400f-9ccc-ab5be2638256'
+  },
 };
 
 const prod = {
-  STRIPE_KEY: "pk_test_v1amvR35uoCNduJfkqGB8RLD",
+  STRIPE_KEY: 'pk_test_v1amvR35uoCNduJfkqGB8RLD',
   apiGateway: {
-    REGION: "us-east-1",
-    URL: "https://di7nan8s01.execute-api.us-east-1.amazonaws.com/Prod"
+    REGION: 'us-east-1',
+    URL: 'https://di7nan8s01.execute-api.us-east-1.amazonaws.com/Prod',
   },
   cognito: {
-    REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_f7f94AC0F",
-    APP_CLIENT_ID: "6u3ukkptov5uksoklarhcmsul8",
-    IDENTITY_POOL_ID: "us-east-1:8a592fb0-5fd7-400f-9ccc-ab5be2638256"
-  }
+    REGION: 'us-east-1',
+    USER_POOL_ID: 'us-east-1_f7f94AC0F',
+    APP_CLIENT_ID: '6u3ukkptov5uksoklarhcmsul8',
+    IDENTITY_POOL_ID: 'us-east-1:8a592fb0-5fd7-400f-9ccc-ab5be2638256'
+  },
 };
 
 // Default to dev if not set
@@ -34,5 +34,5 @@ const config = process.env.REACT_APP_STAGE === 'prod'
 export default {
   // Add common config values here
   MAX_ATTACHMENT_SIZE: 5000000,
-  ...config
+  ...config,
 };
